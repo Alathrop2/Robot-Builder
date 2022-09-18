2278747cd061b749ba10d06510c3e8d140372fcb
Alathrop2 <98856957+Alathrop2@users.noreply.github.com>
2022-09-18 10:32:05 -0600

    done with robot

--- robot_builder.py
+++ robot_builder.py
@@ -5,26 +5,29 @@ import turtle as t
 
 
 def rectangle(horizontal, vertical, color):
+
     t.pendown()
     t.pensize(1)
     t.color(color)
     t.begin_fill()
+
     for counter in range(1, 3):
         t.forward(horizontal)
         t.right(90)
         t.forward(vertical)
         t.right(90)
+
     t.end_fill()
     t.penup()
     t.speed('fast')
     t.bgcolor('Dodger blue')
 
-
-# feet
+    # feet
 t.goto(-100, -150)
 rectangle(50, 20, 'blue')
 t.goto(-30, -150)
 rectangle(50, 20, 'blue')
+
 
 # legs
 t.goto(-25, -50)
@@ -63,9 +66,11 @@ rectangle(5, 5, 'blacl')
 t.goto(-40, 155)
 rectangle(5, 5, 'black')
 
+
 # mouth
+
 t.goto(-65, 135)
 rectangle(40, 5, 'black')
 
-t.done()
+
 t.hideturtle()
